a68c32a774ef39611ca24734d15d2c84c9abfef7
lianewalter <liane.walter@thoughtworks.com>
2019-10-14 16:59:48 +0200

    Story 2.1 | added Movie entity class and added MovieController constructor

--- src/main/java/com/twu/biblioteca/movie/MovieController.java
+++ src/main/java/com/twu/biblioteca/movie/MovieController.java
@@ -1,6 +1,17 @@
 package com.twu.biblioteca.movie;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieController {
 
+    private List<Movie> movies;
 
+    public MovieController() {
+        this.movies = new ArrayList<>();
+
+        this.movies.add(new Movie("Frozen", "2013","Chris Buck, Jennifer Lee", "unrated"));
+        this.movies.add(new Movie("Tangled", "2010", "Nathan Greno, Byron Howard", "7" ));
+        this.movies.add(new Movie("Moana", "2016","Ron Clements, John Musker", "unrated"));
+    }
 }
