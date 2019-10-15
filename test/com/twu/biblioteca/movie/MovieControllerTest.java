f61d6f672fcf28cadb31f6f9996b31a96617f82c
lianewalter <liane.walter@thoughtworks.com>
2019-10-15 08:45:11 +0200

    Story 2.2 | added shouldCheckOutMovieCorrectly method to test the check-out process for movies

--- test/com/twu/biblioteca/movie/MovieControllerTest.java
+++ test/com/twu/biblioteca/movie/MovieControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -12,9 +14,8 @@ import static org.junit.Assert.*;
 
 public class MovieControllerTest {
     private ByteArrayOutputStream outContent;
-    private MovieController movieController;
 
-
+    private List<Movie> mockMovies;
 
 
 
@@ -22,14 +23,31 @@ public class MovieControllerTest {
     public void setUp() throws Exception {
         outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
-        movieController = new MovieController();
+
+        mockMovies = new ArrayList<>();
+
+        mockMovies.add(new Movie("Frozen", "2013","Chris Buck, Jennifer Lee", "unrated"));
+        mockMovies.add(new Movie("Tangled", "2010", "Nathan Greno, Byron Howard", "7" ));
+
     }
 
 
     @Test
     public void shouldShowMovieList(){
+        MovieController movieController = new MovieController();
+
         movieController.showMovieList();
         assertThat(outContent.toString(), containsString("Frozen"));
+    }
+
+
+    @Test
+    public void shouldCheckOutMovieCorrectly(){
+       MovieController movieController = new MovieController();
+       movieController.checkOutMovie(mockMovies.get(0).getName());
+
+       movieController.showMovieList();
+       assertThat(outContent.toString(),not((containsString(mockMovies.get(0).getName()))));
     }
 
 }
