38b696aceeee2b2b3fea97e0d9f8e61e4f1b6c53
lianewalter <liane.walter@thoughtworks.com>
2019-10-15 11:43:44 +0200

    Story 2.3 | implemented login method in UserController

--- src/main/java/com/twu/biblioteca/user/UserController.java
+++ src/main/java/com/twu/biblioteca/user/UserController.java
@@ -1,4 +1,24 @@
 package com.twu.biblioteca.user;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserController {
+
+
+    private List<User> users;
+
+    public UserController() {
+
+        this.users = new ArrayList<>();
+        this.users.add(new User("123-4567", "secret"));
+    }
+
+
+    public boolean login(String libraryNumber, String password){
+        List<User> userList = this.users.stream().filter(user -> user.getLibraryNumber().equals(libraryNumber) && user.getPassword().equals(password)).collect(Collectors.toList());
+
+        return !userList.isEmpty();
+    }
 }
